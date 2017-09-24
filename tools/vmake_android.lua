fbb2f9eacc093b624adb9a4a49c6e14a5eb10dc4
liangjun <liangjun@aurogon.com>
2017-09-24 23:55:40 +0800

    port cmake/android.toolchain.cmake

--- tools/vmake_android.lua
+++ tools/vmake_android.lua
@@ -1,13 +1,101 @@
 -- vmake_android.lua
 
+-- see : ndk/build/cmake/android.toolchain.cmake
+
 local _TOOLCHAINS =
-	{ ['arm']    = { name='arm-linux-androideabi', prefix='arm-linux-androideabi' }
-	, ['arm64']  = { name='aarch64-linux-android', prefix='aarch64-linux-android' }
-	, ['x86']    = { name='x86', prefix='i686-linux-android' }
-	, ['x86_64'] = { name='x86_64', prefix='x86_64-linux-android' }
-	, ['mips']   = { name='mipsel-linux-android', prefix='mipsel-linux-android' }
-	, ['mips64'] = { name='mips64el-linux-android', prefix='mips64el-linux-android' }
+	{ ['arm'] =
+		{ name = 'arm-linux-androideabi'
+		, prefix = 'arm-linux-androideabi'
+		, abi = 'armeabi-v7a'
+		}
+	, ['arm64'] =
+		{ name = 'aarch64-linux-android'
+		, prefix = 'aarch64-linux-android'
+		, abi = 'arm64-v8a'
+		}
+	, ['x86'] =
+		{ name = 'x86'
+		, prefix = 'i686-linux-android'
+		, abi = 'x86'
+		}
+	, ['x86_64'] =
+		{ name = 'x86_64'
+		, prefix = 'x86_64-linux-android'
+		, abi = 'x86_64'
+		}
+	, ['mips'] =
+		{ name = 'mipsel-linux-android'
+		, prefix = 'mipsel-linux-android'
+		, abi = 'mips'
+		}
+	, ['mips64'] =
+		{ name = 'mips64el-linux-android'
+		, prefix='mips64el-linux-android'
+		, abi='mips64'
+		}
 	}
+
+local _ABI_FLAGS =
+	{ ['armeabi'] =
+		{ cflags = {'-march=armv5te', '-mtune=xscale', '-msoft-float'}
+		}
+	, ['armeabi-v7a'] =
+		{ cflags = {}
+		, linker_flags = {}
+		}
+	, ['mips'] =
+		{ cflags = {}
+		}
+	}
+
+function get_android_cflags(ndk_root, arch, api_level)
+	local toolchain = _TOOLCHAINS[arch]
+	if not toolchain then error('not support arch:'..tostring(arch)) end
+	local sysroot = path_concat(ndk_root, 'platforms', 'android-'..api_level, 'arch-'..arch)
+	local flags = 
+		{ '-DANDROID'
+		, '-ffunction-sections'
+		, '-funwind-tables'
+		, '-fstack-protector-strong'
+		, '-no-canonical-prefixes'
+		, '--sysroot='..sysroot
+		}
+
+	if toolchain.abi=='armeabi' then
+		flags = array_pack(flags, '-march=armv5te', '-mtune=xscale', '-msoft-float')
+	elseif toolchain.abi=='armeabi-v7a' then
+		flags = array_pack(flags, '-march=armv7-a', '-mfloat-abi=softfp', '-mfpu=vfpv3-d16')
+	elseif toolchain.abi=='mips' then
+		flags = array_pack(flags, '-mips32')
+	end
+
+	-- http://b.android.com/222239
+	-- http://b.android.com/220159 (internal http://b/31809417)
+	-- x86 devices have stack alignment issues.
+	if arch=='x86' then table.insert(flags, '-mstackrealign') end
+
+	return flags
+end
+
+function get_android_linker_flags(arch)
+	local toolchain = _TOOLCHAINS[arch]
+	if not toolchain then error('not support arch:'..tostring(arch)) end
+	local flags =
+		{ '-Wl,--build-id'
+		, '-Wl,--warn-shared-textrel'
+		, '-Wl,--fatal-warnings'
+		}
+
+	if toolchain.abi=='armeabi-v7a' then table.insert(flags, '-Wl,--fix-cortex-a8') end
+
+	return flags
+end
+
+function get_android_abi(arch)
+	local toolchain = _TOOLCHAINS[arch]
+	if not toolchain then error('not support arch:'..tostring(arch)) end
+	return toolchain.abi
+end
 
 function get_gcc_toolchain_binprefix(ndk_root, arch, gcc_ver)
 	local host_os = (vlua.OS=='windows') and 'windows' or 'linux'
@@ -21,10 +109,6 @@ function get_gcc_toolchain_binprefix(ndk_root, arch, gcc_ver)
 			, 'bin'
 			, toolchain.prefix..'-'
 			)
-end
-
-function get_android_sysroot(ndk_root, arch, api_level)
-	return path_concat(ndk_root, 'platforms', 'android-'..api_level, 'arch-'..arch)
 end
 
 function vmake_target_with_all_archs(ndk_root, api_level)
